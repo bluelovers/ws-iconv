2856c30e219640632c9baf3216812e0b19905dc1
bluelovers <codelovers@users.sourceforge.net>
2017-12-10 15:50:16 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	lib/fs.ts

---
+++ lib/fs.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by user on 2017/12/10/010.
+ */
+
+import { fn, upath, IPath } from '../core';
+
+let r_vaild = /[\\\?\/\!'"\:\<\>\*\|]+/g;
+
+export function vaildNameEntry<T = string>(name: T): T
+{
+	return r_vaild.test(name.toString()) ? null : name;
+}
+
+export function filterNameEntry<T = string>(name: T): string
+{
+	return name.toString().replace(r_vaild, '');
+}
+
+fn.vaildNameEntry = vaildNameEntry;
+fn.filterNameEntry = filterNameEntry;
+
+// @ts-ignore
+//console.log(upath.vaildNameEntry('a/b.test'), upath.filterNameEntry('a/b.test'));
+
+// @ts-ignore
+export default exports;
