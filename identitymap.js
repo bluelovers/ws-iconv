342a0b5bb10a85f3768a0b3d8cd68cf74f7b589d
Colten Jackson <jazzyjackson@gmail.com>
2019-06-27 14:59:20 -0500

    just fixed tabs to spaces

--- identitymap.js
+++ identitymap.js
@@ -10,46 +10,46 @@ const os = require('os')
 const child_process = require('child_process')
 
 let matchKeyValue = (new RegExp).compile(
-		'^'           +// Only match at the beginning of line
-	 	'([\\w\\.]+)' +// (Capture) at least one+ word character \w [and] dots \.
-	  '[^-\\d]*'    +// Skip any number* of everything thats not^ a hyphen - [or] a digit \d
-		'(-?\\d+)'    +// Capture any number of digits, optionally? with a hyphen for '-1' and '-2'
+    '^'           +// Only match at the beginning of line
+    '([\\w\\.]+)' +// (Capture) at least one+ word character \w [and] dots \.
     '[^-\\d]*'    +// Skip any number* of everything thats not^ a hyphen - [or] a digit \d
-		'$'            // end of line
+    '(-?\\d+)'    +// Capture any number of digits, optionally? with a hyphen for '-1' and '-2'
+    '[^-\\d]*'    +// Skip any number* of everything thats not^ a hyphen - [or] a digit \d
+    '$'            // end of line
 )
 
 function splitKeyValue(line){
-	return line.replace(matchKeyValue, (match, groupname, gid) => [gid, groupname]).split(',')
+  return line.replace(matchKeyValue, (match, groupname, gid) => [gid, groupname]).split(',')
 }
 
 function neitherCommentNorBlank(line){
-	return line && line[0] != '#'
+  return line && line[0] != '#'
 }
 
 function rollup(accumulator, current){
-	return Object.assign(accumulator, {[current.shift()]: current.shift()})
+  return Object.assign(accumulator, {[current.shift()]: current.shift()})
 }
 
 function parse(bufferinput){
-	return bufferinput
-		.toString()
-		.split(os.EOL)
-		.filter(neitherCommentNorBlank)
-		.map(splitKeyValue)
-		.reduce(rollup, new Object)
+  return bufferinput
+    .toString()
+    .split(os.EOL)
+    .filter(neitherCommentNorBlank)
+    .map(splitKeyValue)
+    .reduce(rollup, new Object)
 }
 
 function gids(){
-	return parse(fs.readFileSync('/etc/group'))
+  return parse(fs.readFileSync('/etc/group'))
 }
 
 function ids(){
-	switch(process.platform){
-		case 'darwin': 
-			return parse(child_process.execSync('dscl . -list /Users UniqueID'))
-		case 'linux':
-		case 'freebsd': 
-			return parse(fs.readFileSync('/etc/passwd'))
-	}
+  switch(process.platform){
+    case 'darwin': 
+      return parse(child_process.execSync('dscl . -list /Users UniqueID'))
+    case 'linux':
+    case 'freebsd': 
+      return parse(fs.readFileSync('/etc/passwd'))
+  }
 }
 module.exports = {gid: gids(), id: ids()}
