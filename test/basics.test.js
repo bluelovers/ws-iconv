01a5564f5c9b8b773281f877d0e71380734a12ca
Colten Jackson <jazzyjackson@gmail.com>
2019-06-27 16:33:53 -0500

    Updated the test to call it correctly at least, needs more tests

--- test/basics.test.js
+++ test/basics.test.js
@@ -1,26 +1,32 @@
 const {expect} = require('chai')
 const {extrastat, defaults} = require('../extrastat')
 
+extrastatAsync = require('util').promisify(extrastat)
+
 describe('extrastatSync Default Behavoir', () => {
 	it('Loads a default configuration from disk', () => {
 		expect(defaults).to.deep.equal(require('../.config.json'))
 	})
 
-	it('Returns an object containing a key for each options set to true', () => {
-		let example = extrastat(process.cwd(), (err, stat) => {
-			expect(example).to.have.all.keys([
+	it('Returns an object containing a key for each options set to true', done => {
+		extrastatAsync(process.cwd()).then(stat => {
+			expect(stat).to.have.all.keys([
 				'filestat',
 				'filemode',
 				'filename',
 				'pathname',
-				'mimetype'
+				'mimetype',
 			])
 
-			expect(example).to.not.have.any.keys([
+			expect(stat).to.not.have.any.keys([
+				'role',
 				'ownername',
 				'groupname',
-				'role'
+				'parents',
+				'children',
+				'siblings'
 			])
-		})
+
+		}).then(done).catch(done)
 	})
 })
