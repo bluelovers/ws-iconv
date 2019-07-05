10d5d31ffaa1d92af1af671951f81698847154b2
Colten Jackson <jazzyjackson@gmail.com>
2019-07-04 18:07:18 -0700

    simplified the promise syntax in the tests, added an example directory to test empty directories

--- test/basics.test.js
+++ test/basics.test.js
@@ -9,7 +9,7 @@ describe('extrastatSync Default Behavoir', () => {
 		expect(defaults).to.deep.equal(require('../.config.json'))
 	})
 
-	it('Returns an object containing a key for each options set to true', done => {
+	it('Returns an object containing a key for each options set to true', () => 
 		extrastatAsync(process.cwd()).then(stat => {
 			expect(stat).to.have.all.keys([
 				'filemode',
@@ -27,29 +27,28 @@ describe('extrastatSync Default Behavoir', () => {
 				'children',
 				'siblings'
 			])
-
-		}).then(done).catch(done)
-	})
+		})
+	)
 })
 
 describe('options for children and siblings', () => {
-	it('lets you get the children of the current working directory', done => {
+	it('lets you get the children of the current working directory', () => 
 		extrastatAsync(process.cwd(), {children: true}).then(stat => {
 			expect(stat.children).to.deep.include({
 			 filename: 'extrastat.js',
        pathname: path.resolve('extrastat.js'),
        mimetype: 'text/plain',
 			})
-		}).then(done).catch(done)
-	})
+		})
+	)
 
-	it('lets you request children on a nondirectory, returning null', done => {
+	it('lets you request children on a nondirectory, returning null', () => 
 		extrastatAsync('package.json', {children: true}).then(stat => {
 			expect(stat.children).to.equal(null)
-		}).then(done).catch(done)
-	})
+		})
+	)
 
-	it('gives you the siblings of a file', done => {
+	it('gives you the siblings of a file', () =>
 		extrastatAsync('package.json', {children: true, siblings: true}).then(stat => {
 			expect(stat.children).to.equal(null)
 			expect(stat.siblings).to.deep.include({
@@ -57,15 +56,15 @@ describe('options for children and siblings', () => {
        pathname: path.resolve('extrastat.js'),
        mimetype: 'text/plain',
 			})
-		}).then(done).catch(done)
-	})
+		})
+	)
 
-	it('returns an empty array as children when it encounters an empty directory', done => {
+	it('returns an empty array as children when it encounters an empty directory', () => 
 		// fs.mkdirSync('exampleEmpty')
 		// fs.rmdirSync('exampleEmpty')
 		extrastatAsync('exampleEmpty', {children: true, siblings: true}).then(stat => {
 			expect(stat.children).to.be.an("array")
 			expect(stat.children.length).to.equal(0)
-		}).then(done).catch(done)
-	})
+		})
+	)
 })
