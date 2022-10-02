b91503202f23d6be657ee0c13009f55dd08ef131
bluelovers <codelovers@users.sourceforge.net>
2022-10-02 08:57:47 +0800

    .

--- packages/@lazy-node/is-writeable-path/index.js
+++ packages/@lazy-node/is-writeable-path/index.js
@@ -2,41 +2,43 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isWritableDirectorySync = exports.isWritableDirectoryAsync = exports.isWritableFileSync = exports.isWritableFileAsync = exports.isWritableSync = exports.isWritableAsync = void 0;
 const fs_1 = require("fs");
+const fs_stat_1 = require("fs-stat");
+const RW_OK = fs_1.constants.W_OK | fs_1.constants.R_OK;
 function isWritableAsync(target) {
-    return fs_1.promises.access(target, fs_1.constants.W_OK | fs_1.constants.R_OK)
+    return fs_1.promises.access(target, RW_OK)
         .then(r => { var _a; return (_a = r) !== null && _a !== void 0 ? _a : true; });
 }
 exports.isWritableAsync = isWritableAsync;
 function isWritableSync(target) {
     var _a;
     try {
-        return (_a = (0, fs_1.accessSync)(target, fs_1.constants.W_OK | fs_1.constants.R_OK)) !== null && _a !== void 0 ? _a : true;
+        return (_a = (0, fs_1.accessSync)(target, RW_OK)) !== null && _a !== void 0 ? _a : true;
     }
     catch (err) { }
 }
 exports.isWritableSync = isWritableSync;
 function isWritableFileAsync(target) {
-    return fs_1.promises.stat(target)
-        .then((stat) => {
-        return stat.isFile() && isWritableAsync(target);
-    });
+    return (0, fs_stat_1.fsStatExists)(target, {
+        onlyFiles: true,
+    }).then(() => isWritableAsync(target));
 }
 exports.isWritableFileAsync = isWritableFileAsync;
 function isWritableFileSync(target) {
-    const stat = (0, fs_1.statSync)(target);
-    return stat.isFile() && isWritableSync(target);
+    return (0, fs_stat_1.fsStatExistsSync)(target, {
+        onlyFiles: true,
+    }) && isWritableSync(target);
 }
 exports.isWritableFileSync = isWritableFileSync;
 function isWritableDirectoryAsync(target) {
-    return fs_1.promises.stat(target)
-        .then((stat) => {
-        return stat.isDirectory() && isWritableAsync(target);
-    });
+    return (0, fs_stat_1.fsStatExists)(target, {
+        onlyDirectories: true,
+    }).then(() => isWritableAsync(target));
 }
 exports.isWritableDirectoryAsync = isWritableDirectoryAsync;
 function isWritableDirectorySync(target) {
-    const stat = (0, fs_1.statSync)(target);
-    return stat.isDirectory() && isWritableSync(target);
+    return (0, fs_stat_1.fsStatExistsSync)(target, {
+        onlyDirectories: true,
+    }) && isWritableSync(target);
 }
 exports.isWritableDirectorySync = isWritableDirectorySync;
 exports.default = isWritableSync;
--- packages/@lazy-node/is-writeable-path/index.ts
+++ packages/@lazy-node/is-writeable-path/index.ts
@@ -1,13 +1,16 @@
 import {
 	promises as fsPromises,
-	constants, PathLike,
+	constants,
+	PathLike,
 	accessSync,
-	statSync,
 } from 'fs';
+import { fsStatExists, fsStatExistsSync } from 'fs-stat';
+
+const RW_OK = constants.W_OK | constants.R_OK;
 
 export function isWritableAsync(target: PathLike): Promise<boolean>
 {
-	return fsPromises.access(target, constants.W_OK | constants.R_OK)
+	return fsPromises.access(target, RW_OK)
 		.then(r => r as any ?? true)
 	;
 }
@@ -16,39 +19,37 @@ export function isWritableSync(target: PathLike): boolean
 {
 	try
 	{
-		return accessSync(target, constants.W_OK | constants.R_OK) as any ?? true
+		return accessSync(target, RW_OK) as any ?? true
 	}
 	catch (err) {}
 }
 
 export function isWritableFileAsync(target: PathLike)
 {
-	return fsPromises.stat(target)
-		.then((stat) => {
-			return stat.isFile() && isWritableAsync(target)
-		})
+	return fsStatExists(target as any, {
+		onlyFiles: true,
+	}).then(() => isWritableAsync(target))
 }
 
 export function isWritableFileSync(target: PathLike)
 {
-	const stat = statSync(target)
-
-	return stat.isFile() && isWritableSync(target)
+	return fsStatExistsSync(target as any, {
+		onlyFiles: true,
+	}) && isWritableSync(target)
 }
 
 export function isWritableDirectoryAsync(target: PathLike)
 {
-	return fsPromises.stat(target)
-		.then((stat) => {
-			return stat.isDirectory() && isWritableAsync(target)
-		})
+	return fsStatExists(target as any, {
+		onlyDirectories: true,
+	}).then(() => isWritableAsync(target))
 }
 
 export function isWritableDirectorySync(target: PathLike)
 {
-	const stat = statSync(target)
-
-	return stat.isDirectory() && isWritableSync(target)
+	return fsStatExistsSync(target as any, {
+		onlyDirectories: true,
+	}) && isWritableSync(target)
 }
 
 export default isWritableSync
